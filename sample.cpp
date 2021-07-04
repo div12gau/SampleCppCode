0204c5520f050c404ede2854aef85cb620e5664f
Divyanshu Srivastava <divyanshu.srivastava@soroco.com>
2021-07-05 00:32:27 +0530

    First Commit

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    int n;
+    //take input from user
+    cin>>n;
+    cout<<"The value is "<<n<<endl;
+    return 0;
+}
